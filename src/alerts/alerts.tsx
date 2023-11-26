c08558824cc975ef1df5294667d2d6a5d5631516
DarrenC07 <darrenchen2002@gmail.com>
2023-11-26 00:09:44 -0500

    Timer along with corresponding dog animations
    - first created timer
    - found animations for 4 states for a dog (excited, happy, resting, angry)
    - happy when timer starts
    - resting after halfway mark for timer
    - angry when it hits 0
    - reset timer to make it excited
    - start timer again and it becomes happy
    - cycle repeats
    - code compiles successfully but google chrom says error

--- src/alerts/alerts.tsx
+++ src/alerts/alerts.tsx
@@ -1,52 +1,87 @@
 import React, { useState, useEffect } from 'react';
-import { createRoot } from 'react-dom/client';
-import './alerts.css'
+import '@lottiefiles/lottie-player';
+import './alerts.css';
+import angryAnimation from './angry.json';
+import restingAnimation from './resting.json';
+import happyAnimation from './happy.json';
+import excitedAnimation from './excited.json';
 
-const test = <img src="icon.png" style={{ maxWidth: '60px' }}/>
 
-const test2 = <p>Hello World</p>;
 
-const root = document.createElement('div');
-document.body.appendChild(root);
-
-const rootElement = createRoot(root);
-rootElement.render(test);
+const animations = {
+  angry: angryAnimation,
+  resting: restingAnimation,
+  happy: happyAnimation,
+  excited: excitedAnimation,
+};
 
 const Popup: React.FC = () => {
-    const [buttonText, setButtonText] = useState('Start Timer');
-    const [timer, setTimer] = useState(10);
-  
-    const handleClick = () => {
-      // Reset the timer to 10 seconds
-      setTimer(10);
-      // Start the countdown
+  const [buttonText, setButtonText] = useState('Start Timer');
+  const [timer, setTimer] = useState(10);
+  const [isPaused, setIsPaused] = useState(true);
+  const [currentAnimation, setCurrentAnimation] = useState(animations.happy);
+
+  const handleClick = () => {
+    if (isPaused) {
       setButtonText('Timer is running');
-    };
-  
-    useEffect(() => {
-      let countdownInterval: NodeJS.Timeout;
-  
-      if (timer > 0 && buttonText === 'Timer is running') {
-        countdownInterval = setInterval(() => {
-          setTimer((prevTimer) => prevTimer - 1);
-        }, 1000);
-      } else if (timer === 0) {
-        setButtonText('Timer complete!');
-        // Add your logic when the timer reaches 0
+      setIsPaused(false);
+      if (timer > 5) {
+        setCurrentAnimation(animations.happy);
+      } else {
+        setCurrentAnimation(animations.resting);
       }
-  
-      return () => {
-        clearInterval(countdownInterval);
-      };
-    }, [timer, buttonText]);
-  
-    return (
-      <div>
-        <button onClick={handleClick}>{buttonText}</button>
-        <p>Time remaining: {timer} seconds</p>
-      </div>
-    );
+    } else {
+      setButtonText('Timer paused');
+      setIsPaused(true);
+      setCurrentAnimation(animations.resting);
+    }
   };
-  
-  // Render the React component in the popup.html
-  rootElement.render(<Popup />, document.getElementById('root'));
+
+  const handleReset = () => {
+    setTimer(10);
+    setButtonText('Start Timer');
+    setIsPaused(false);
+    setCurrentAnimation(animations.excited);
+  };
+
+  useEffect(() => {
+    let countdownInterval: NodeJS.Timeout;
+
+    if (timer > 0 && buttonText === 'Timer is running') {
+      countdownInterval = setInterval(() => {
+        setTimer((prevTimer) => prevTimer - 1);
+        if (timer <= 5) {
+          setCurrentAnimation(animations.resting);
+        }
+      }, 1000);
+    } else if (timer === 0) {
+      setButtonText('Timer complete!');
+      setCurrentAnimation(animations.angry);
+      // Add your logic when the timer reaches 0
+    }
+
+    return () => {
+      clearInterval(countdownInterval);
+    }
+  }, [timer, buttonText]);
+
+  return (
+    <div>
+      {/* Use lottie-player with the specified attributes */}
+      <lottie-player
+        autoplay
+        controls
+        loop
+        mode="normal"
+        src={currentAnimation}
+        style={{ width: '320px' }}
+      ></lottie-player>
+
+      <button onClick={handleClick}>{buttonText}</button>
+      <button onClick={handleReset}>Reset Timer</button>
+      <p>Time remaining: {timer} seconds</p>
+    </div>
+  );
+};
+
+export default Popup;
--- src/options/options.tsx
+++ src/options/options.tsx
@@ -1,11 +1,4 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
 import './options.css';
 
 const test = <p>Hello World</p>;
-
-const root = document.createElement('div');
-document.body.appendChild(root);
-
-const rootElement = createRoot(root);
-rootElement.render(test);
