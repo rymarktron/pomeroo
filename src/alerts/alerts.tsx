7d11fdb44fa717e57d34c9bfb50d50ea6d3b7d90
Steven Zhang <76230272+StevenO7@users.noreply.github.com>
2023-11-25 22:06:34 -0500

    reverting
    Taking a step back to redo some things

--- src/alerts/alerts.tsx
+++ src/alerts/alerts.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import './alerts.css'
 
@@ -11,3 +11,42 @@ document.body.appendChild(root);
 
 const rootElement = createRoot(root);
 rootElement.render(test);
+
+const Popup: React.FC = () => {
+    const [buttonText, setButtonText] = useState('Start Timer');
+    const [timer, setTimer] = useState(10);
+  
+    const handleClick = () => {
+      // Reset the timer to 10 seconds
+      setTimer(10);
+      // Start the countdown
+      setButtonText('Timer is running');
+    };
+  
+    useEffect(() => {
+      let countdownInterval: NodeJS.Timeout;
+  
+      if (timer > 0 && buttonText === 'Timer is running') {
+        countdownInterval = setInterval(() => {
+          setTimer((prevTimer) => prevTimer - 1);
+        }, 1000);
+      } else if (timer === 0) {
+        setButtonText('Timer complete!');
+        // Add your logic when the timer reaches 0
+      }
+  
+      return () => {
+        clearInterval(countdownInterval);
+      };
+    }, [timer, buttonText]);
+  
+    return (
+      <div>
+        <button onClick={handleClick}>{buttonText}</button>
+        <p>Time remaining: {timer} seconds</p>
+      </div>
+    );
+  };
+  
+  // Render the React component in the popup.html
+  rootElement.render(<Popup />, document.getElementById('root'));
